23462b1bb09338842a85961ceb803079154cbd11
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-11-29 14:31:05 -0800

    edit date functionality
    edit how date functions. also add next, prev, today, buttons

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { today, previous, next } from "../utils/date-time";
+import {Link} from "react-router-dom"
 
 /**
  * Defines the dashboard page.
@@ -11,6 +13,7 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  if (!date) date = today()
 
   useEffect(loadDashboard, [date]);
 
@@ -23,13 +26,24 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  
+
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date: {date}</h4>
+        <h4 className="mb-0">Reservations for date:</h4>
       </div>
       <ErrorAlert error={reservationsError} />
+      <Link to={`/dashboard?date=${previous(date)}`}> 
+        <button onClick = {() => {date = previous(date)}}className="btn btn-secondary" type="button">Previous</button>
+      </Link>
+      <Link to={`/dashboard?date=${today()}`}> 
+        <button onClick= {() => {date = today()}} className="btn btn-primary" type="button">Today</button>
+      </Link>
+      <Link to={`/dashboard?date=${next(date)}`}> 
+        <button onClick = {() => {date = next(date)}}className="btn btn-secondary" type="button">Next</button>
+      </Link>
       {JSON.stringify(reservations)}
     </main>
   );
