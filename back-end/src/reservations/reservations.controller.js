e102ea06bfe19490b87090c3806990de6b53ee29
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-11-29 14:29:56 -0800

    Update reservations.controller.js
    add and edit create, read, exists, functions

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,11 +1,34 @@
 const service = require("./reservations.service")
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const { reset } = require("nodemon")
 
 /**
  * List handler for reservation resources
  */
+async function reservationExists(req, res, next) {
+  const {reservation_id} = req.params
+  try{
+    const reservation = await service.read(reservation_id)
+    if (reservation){
+      res.locals.reservation = reservation
+      next()
+    } else{
+      next({status: 404, message: `reservation id: ${reservation_id} does not exist`})
+    }
+  }catch(err){
+    console.error(err)
+  }
+}
+
+function read(req, res){
+  const reservation = res.locals.reservation
+  console.log("reservation:", reservation)
+  res.json(reservation)
+}
+
 async function list(req, res) {
   const {date} = req.query
+  console.log("date:", date)
   if (date){
     try {
       const data = await service.listByDate(date)
@@ -13,20 +36,25 @@ async function list(req, res) {
     } catch (err){
       console.error(err)
     }
-}
+  } 
+  return;
 }
 
 async function create(req, res, next){
+  const data = req.body.data
+  console.log("req.body.data:", req.body.data)
   try{
-    const data = await service.create(req.body.data)
-    res.status(201).json({ data })
+    const results = await service.create(data)
+    console.log("results:", results)
+    res.status(201).json({ results })
   } catch(err){
     console.error(err)
-    return;
+    res.sendStatus()
   }
 }
 
 module.exports = {
+  read: [asyncErrorBoundary(reservationExists), read],
   list: [asyncErrorBoundary(list)],
   create: [asyncErrorBoundary(create)]
 }
