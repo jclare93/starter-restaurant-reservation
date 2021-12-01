a47324e8b2e91bf69c9a4876052c1fdac7164e6f
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-11-30 16:02:24 -0800

    Create validateDateTime.js
    add date and time validationn

---
+++ back-end/src/errors/validateDateTime.js
@@ -0,0 +1,23 @@
+function validateDateTime(req, res, next){
+    const {data} = req.body
+    const reservation = data
+    const formatDate = new Date(reservation.reservation_date)
+    console.log("formatDate:", formatDate)
+    const reservationDay = formatDate.getDay()
+    console.log("reservationDay:", reservationDay)
+    const now = new Date()
+    if(reservationDay == 1){
+        return next({
+            status: 400,
+            message: "sorry. closed on tuesdays."
+        })
+    }
+    if(formatDate<now){
+        return next({
+            status: 400, 
+            message: "sorry. reservations must be in the future."
+        })
+    }
+    next()
+}
+module.exports= validateDateTime
