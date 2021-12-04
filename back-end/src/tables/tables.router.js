24bf529aa274bdd243680170fae33535ff34b0f9
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-04 15:23:34 -0800

    Update tables.router.js
    add delete to table router

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -12,7 +12,7 @@ router
     .route("/").get(controller.list).post(controller.create).all(methodNotAllowed)
 
 router
-     .route("/:table_id/seat").put(controller.update).all(methodNotAllowed)
+     .route("/:table_id/seat").put(controller.update).delete(controller.finishReservation).all(methodNotAllowed)
 
 router 
     .route("/:table_id").get(controller.read).all(methodNotAllowed)
