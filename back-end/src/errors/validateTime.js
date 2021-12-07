ba9abb94c19c20baf48450e6543cc6ecfb27bd8d
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-07 09:27:43 -0800

    Update validateTime.js
    remove console.logs

--- back-end/src/errors/validateTime.js
+++ back-end/src/errors/validateTime.js
@@ -5,13 +5,12 @@ function validateTime(req, res, next){
     let reservationTime = reservation.reservation_time.split('')
     let reservationHours = reservationTime.slice(0,2)
     reservationHours = parseInt(reservationHours.join(''))
-    console.log("reservationHours:", reservationHours)
+    
     let reservationMinutes = reservationTime.slice(3)
     reservationMinutes = reservationMinutes.join('')
     reservationMinutes = reservationMinutes/100
-   console.log("reservationMinutes:", reservationMinutes) 
     const reservationTimeFormat = reservationHours+reservationMinutes
-    console.log("reservationTimeFormat:", reservationTimeFormat)
+    
 
     if(10.3 > reservationTimeFormat ||  reservationTimeFormat > 21.3){
         return next({
