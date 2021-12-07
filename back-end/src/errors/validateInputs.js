8ec8f62c50f60d655c3f8fc1cea520099928328d
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-07 09:31:30 -0800

    Update validateInputs.js

--- back-end/src/errors/validateInputs.js
+++ back-end/src/errors/validateInputs.js
@@ -3,21 +3,21 @@ function validateInputs(req, res, next){
     const reservation = data
 
     if(!reservation){
-        next({
+        return next({
             status: 400,
             message: 'please send data with your request'
         })
     }
 
     if (!reservation.first_name ||reservation.first_name.length<1){
-        next({
+        return next({
             status: 400,
             message:  "please enter a valid first_name"
         })
     }
 
     if(isNaN(reservation.people) || reservation.people < 1 || !reservation.people){
-        next({
+        return next({
             status: 400,
             message: 'please enter a valid number for people'
         })
@@ -26,25 +26,30 @@ function validateInputs(req, res, next){
     var isValidTime = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(reservation.reservation_time);
 
     if(!reservation.reservation_time|| !isValidTime || reservation.reservation_time.length < 1){
-        next({
+        return next({
             status: 400,
             message: "please enter a valid reservation_time"
         })
     }
     if(!reservation.last_name || reservation.last_name.length<1){
-        next({
+        return next({
             status: 400,
             message: "please enter a valid last_name"
         })
     }
     if(!reservation.mobile_number ||reservation.mobile_number.length<1){
-        next({
+        return next({
             status: 400,
             message: "please enter a valid mobile_number"
         })
     }
 
-    
+    if(reservation.status==="seated"||reservation.status === "finished"){
+        return next({
+            status:400,
+            message: "status seated and finished are not valid statuses for POST"
+        })
+    }
 
     if(!reservation.reservation_date ||reservation.reservation_date.length < 1){
         next({
