0c67ca5318c4536862fe56eb6de39136f679c5cd
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-07 09:27:21 -0800

    add status validation
    validate correct status

---
+++ back-end/src/errors/validateStatus.js
@@ -0,0 +1,22 @@
+function  validateStatus (req, res, next){
+    const {status} = req.body.data
+    
+    if(res.locals.reservation.status === "finished"){
+        return next({
+            status:400,
+            message:"a finished reservation cannot be updated"
+        })
+    }
+    if(status !== "booked" && status !== "seated"){
+        if(status !== "finished"){
+            return next({
+                status: 400,
+                message: `sorry, ${status} is an unknown status`
+            })
+        }
+    }
+    
+    return next()
+}
+
+module.exports = validateStatus;
