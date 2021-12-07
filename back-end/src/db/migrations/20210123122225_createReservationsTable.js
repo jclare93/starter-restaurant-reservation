b42e02d62dca5628d66069e9ee40d2b7cada8bf4
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-07 09:17:39 -0800

    Update 20210123122225_createReservationsTable.js
    create a defaultTo

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -6,7 +6,7 @@ exports.up = function (knex) {
     table.string('mobile_number').notNullable();
     table.date('reservation_date').notNullable();
     table.time('reservation_time').notNullable();
-    table.string('status').notNullable()
+    table.string('status').defaultTo("booked").notNullable()
     table.integer('people').notNullable();
     table.timestamps(true, true);
   });
