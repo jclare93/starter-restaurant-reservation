683a20708c30290efc95c1153b45a41a9a10b86d
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-09 08:38:06 -0800

    Create EditReservation.js
    created edit reservation file

---
+++ front-end/src/reservations/EditReservation.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from "react"
+import {useParams, useHistory} from "react-router-dom"
+import ErrorAlert from "../layout/ErrorAlert"
+import ReservationForm from "./ReservationForm"
+import { updateReservation, readReservation } from "../utils/api"
+import formatReservationDate from "../utils/format-reservation-date"
+ 
+function EditReservation(){
+    const history = useHistory()
+    const {reservation_id}= useParams()
+    const [reservationError, setReservationError] = useState(null)
+    const [reservation, setReservation] = useState({first_name: '', last_name: '', mobile_number: '', 
+    reservation_date: '', reservation_time: '', people: ''})
+
+    useEffect(() => {
+        const abortController = new AbortController();
+    
+        async function getReservation() {
+          setReservationError(null);
+          try {
+            const data = await readReservation(reservation_id, abortController.signal);
+            const formattedData = formatReservationDate(data)
+            setReservation(formattedData);
+          } catch (error) {
+            setReservationError(error);
+          }
+        }
+        getReservation();
+        return () => abortController.abort();
+      }, [reservation_id]);
+
+      const handleSubmit = async(event) => {
+          event.preventDefault()
+          const abortController = new AbortController();
+            setReservationError(null)
+            try {
+                await updateReservation(reservation, abortController.signal);
+            } catch (err) {
+                setReservationError(err)
+                console.error(err)
+                return;
+            }
+            history.goBack()
+        }
+
+    return (
+        <>
+            <ErrorAlert error ={reservationError} />
+            <div className= "container-fluid" >
+                <div className= "row">
+                    <h3>Edit Reservation</h3>
+                </div>
+                {reservation && 
+                <div className = "row">
+                    <ReservationForm reservation = {reservation} setReservation={setReservation} handleSubmit={handleSubmit}/>
+                </div>}
+            </div>
+        </>
+    )
+}
+export default EditReservation;
