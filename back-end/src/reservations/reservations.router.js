499a65a4582a1929a4fe931be370f96abe160138
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-09 08:36:38 -0800

    Update reservations.router.js
    add route for put

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -15,7 +15,7 @@
     .route("/:reservation_id/status").put(controller.updateStatus).all(methodNotAllowed)
  
  router
-     .route("/:reservation_id").get(controller.read).all(methodNotAllowed)
+     .route("/:reservation_id").get(controller.read).put(controller.update).all(methodNotAllowed)
  
  
  
