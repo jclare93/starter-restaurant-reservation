a3ce195d7d21481a9c88d9987c734af0a62e1a3a
jclare93 <85895069+jclare93@users.noreply.github.com>
2021-12-14 07:31:55 -0800

    fix syle path

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -4,7 +4,7 @@ import ErrorAlert from "../layout/ErrorAlert"
 import ReservationForm from "./ReservationForm"
 import { updateReservation, readReservation } from "../utils/api"
 import formatReservationDate from "../utils/format-reservation-date"
-import "./Layout.css";
+import "../layout/Layout.css";
  
 function EditReservation(){
     const history = useHistory()
